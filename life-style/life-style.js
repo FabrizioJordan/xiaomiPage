0ed3d68cc82fe24e9b1954e00a3864da930d36cc
Fabrizio Jordan <fabriorange28@gmail.com>
2023-11-26 20:42:26 -0300

    Created html & js of Life Style created all images of Wearables

---
+++ life-style/life-style.js
@@ -0,0 +1,44 @@
+
+function fetchear(){
+  fetch("/json/life-style/products.json")
+.then(response => response.json())
+.then(datas => iterPuts(datas[0].products));
+
+}
+fetchear();
+
+
+
+function Card({ name, image, url }) {
+try {
+  return `
+      <article class="col-span-12 md:col-span-4 w-full h-full bg-white p-4 flex flex-col justify-center items-center py-8 gap-2 cursor-pointer transition hover-box-shadow-1">
+          <div class="flex flex-col gap-4 justify-center items-center">
+              <h1 class="text-lg sm:text-xl font-semibold">${name}</h1>
+              <a href="${url}" class=" border border-black sm:border-transparent text-base sm:text-sm tracking-wider sm:tracking-widest font-semibold sm:text-white sm:bg-black p-4 rounded-lg transition-colors hover:underline sm:hover:no-underline sm:hover:bg-zinc-500">Leer más<span class="icon-[material-symbols--arrow-right-rounded]"></span></a>
+          </div>
+          <img src="${image}" alt="Image of ${name}">
+      </article>  
+`;
+}
+catch (error) {
+  console.error("Error al retornar en la funciona Card, error ->  " + error)
+}
+}
+
+// PUT articles : PONE LOS articulos
+function putArticles(article) {
+try {
+  document.getElementById("grid__Wearables").innerHTML += Card(article);
+} catch (error) {
+  console.log(`Error en el ${article}, el error: + ${error}`);
+}
+}
+
+// AQUI LE HACEMOS UN FOR A LOS articles PARA LLAMAR A LA FUNCION PUTARTICLES DEPENDIENDO DE CUANTOS articles HAY
+function iterPuts(articles) {
+  for (var i = 0; i < articles.length; i++) {
+    putArticles(articles[i])
+  }
+}
+
